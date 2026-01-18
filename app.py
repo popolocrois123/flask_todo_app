88127ea0ebfc7c6b387099d6fa6bf7e0bbf375c8
popolocrois123 <sora85386@gmail.com>
2026-01-17 20:15:38 -0800

    add app

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-
-
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
 # 空のtodoリスト
-todos = [{"task": "Sample_todo", "done": False}]
+# 宿題：detaleの欄を追加　
+todos = [{"task": "Sample_todo", "detale": None, "done": False}]
 
 @app.route("/")
 def index():
     return render_template("index.html", todos=todos)
 
+
 @app.route("/add", methods=["POST"])
 def add():
-    todo = request.form["todo"]
-    todos.append({"task": todo, "done": False})
+    todo = request.form.get("todo")
+    todo_detale = request.form.get("detale")
+    if todo:  # 空でないときだけ追加
+        if  not todo_detale:
+            todos.append({"task": todo, "detale": None, "done": False})
+        else:
+            todo.append({"task": todo, "detale": todo_detale, "done": False})
+
     return redirect(url_for("index"))
+
+# # 宿題：detaleを追加
+# @app.route("/detale", methods=["POST"])
+# def add_detale():
+#     todo_detale = request.form.get("todo_detale")
+#     todo_index = int(request.form.get("todo_index"))
+#     if todo_detale:  # 空でないときだけ追加
+#         todos[todo_index]["detale"] = todo_detale
+#     return redirect(url_for("index"))
+
 
 @app.route("/edit/<int:index>", methods=["GET", "POST"])
 def edit(index):
@@ -25,6 +41,7 @@ def edit(index):
         return redirect(url_for("index"))
     else:
         return render_template("edit.html", todo=todo, index=index)
+    
 @app.route("/check/<int:index>")    
 def check(index):
     todos[index]["done"] = not todos[index]["done"]
