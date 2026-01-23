9dca97022081f3e287d77ed5c35b14d19ed53969
popolocrois123 <sora85386@gmail.com>
2026-01-23 12:15:22 +0900

    add wtf

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import ValidationError, StringField, PasswordField, SubmitField, TextAreaField, BooleanField
-
+from wtforms.validators import DataRequired
 
 app = Flask(__name__, template_folder="templates")
 
-# フォームを利用する際に必要。セキュリティ対策に必要。
+# フォームを利用する際に必要。セキュリティ対策に必要
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 
@@ -13,17 +13,15 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 継承元のFlaskFormクラスの機能を使えるようになる。
 各フィールドを定義する。ここで定義したフィールドを基にHTMLに配置する。
 """
-class Todo_Registration(FlaskForm):
+class Todo_Form(FlaskForm):
     # todoリストの項目入力フィールド
-    todo = StringField()
+    todo = StringField("Todo", validators=[DataRequired()])
     # todoリストの内容入力フィールド
-    todo_detale = TextAreaField()
+    todo_detale = StringField("詳細")
     # チェックボックスフィールド
     check_box = BooleanField()
     # 送信ボタンフィールド
-    submit = SubmitField()
-
-
+    submit = SubmitField("送信")
 
 
 # 空のtodoリスト
@@ -32,23 +30,31 @@ todos = [{"task": "Sample_todo", "detale": None, "done": False}]
 
 @app.route("/")
 def index():
-    return render_template("index.html", todos=todos)
+    form = Todo_Form()
+    return render_template("index.html", todos=todos, form=form)
 
 
 @app.route("/add", methods=["POST"])
 def add():
-    todo = request.form.get("todo")
-    todo_detale = request.form.get("detale")
-    if todo:  # 空でないときだけ追加
-        if  not todo_detale:
-            todos.append({"task": todo, "detale": None, "done": False})
-        else:
-            todo.append({"task": todo, "detale": todo_detale, "done": False})
+    form = Todo_Form()
+    
+    if form.validate_on_submit():
+        # todo, todo_detaleはこの下に書く
+        todo = form.todo.data
+        todo_detale = form.todo_detale.data
+        if todo:  # 空でないときだけ追加
+            if  not todo_detale:
+                todos.append({"task": todo, "detale": None, "done": False})
+            else:
+                todos.append({"task": todo, "detale": todo_detale, "done": False})
 
-    return redirect(url_for("index"))
+    return render_template("index.html", form=form, todos=todos)
 
-# # 宿題：detaleを追加
-# @app.route("/detale", methods=["POST"])
+# 宿題：detaleを追加
+@app.route("/see_todo", methods=["POST"])
+def see_todo():
+    return render_template("see_todo.html", todos=todos)
+
 # def add_detale():
 #     todo_detale = request.form.get("todo_detale")
 #     todo_index = int(request.form.get("todo_index"))
@@ -57,24 +63,24 @@ def add():
 #     return redirect(url_for("index"))
 
 
-@app.route("/edit/<int:index>", methods=["GET", "POST"])
-def edit(index):
-    todo = todos[index]
-    if request.method == "POST":
-        todo["task"] = request.form["todo"]
-        return redirect(url_for("index"))
-    else:
-        return render_template("edit.html", todo=todo, index=index)
+# @app.route("/edit/<int:index>", methods=["GET", "POST"])
+# def edit(index):
+#     todo = todos[index]
+#     if request.method == "POST":
+#         todo["task"] = request.form["todo"]
+#         return redirect(url_for("index"))
+#     else:
+#         return render_template("edit.html", todo=todo, index=index)
     
-@app.route("/check/<int:index>")    
-def check(index):
-    todos[index]["done"] = not todos[index]["done"]
-    return redirect(url_for("index"))
+# @app.route("/check/<int:index>")    
+# def check(index):
+#     todos[index]["done"] = not todos[index]["done"]
+#     return redirect(url_for("index"))
 
-@app.route("/delete/<int:index>")
-def delete(index):
-    del todos[index]
-    return redirect(url_for("index"))
+# @app.route("/delete/<int:index>")
+# def delete(index):
+#     del todos[index]
+#     return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
