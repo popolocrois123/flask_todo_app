46846f80e1edaf6f8fefc66e785cc18e41907859
popolocrois123 <sora85386@gmail.com>
2026-03-05 09:21:21 +0900

    データベースの導入

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask_wtf import FlaskForm
 from wtforms import DateField, ValidationError, StringField, PasswordField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
 from loguru import logger
-# 宿題
 from flask_bootstrap import Bootstrap
 # 自作クラス
 from form_list import Todo_Form
+# SQLAlchemy
+from sqlalchemy import Boolean, Date, String, Integer, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+import datetime
+
 
 app = Flask(__name__, template_folder="templates")
 
@@ -22,13 +26,40 @@ logger.remove()
 # ログファイル
 logger.add("log_state.log", level="INFO", encoding="utf-8")
 
+
+# ----------------------------
+# SQLAlchemyのデータベースの設定
+# ----------------------------
+
+# ベースクラス
+class Base(DeclarativeBase):
+    pass
+
+# モデル定義
+class TODO_DB(Base):
+    __tablename__ = "todo"
+    id: Mapped[int] = mapped_column(primary_key=True)  # 自動採番
+    task: Mapped[str] = mapped_column(String) 
+    detail: Mapped[str] = mapped_column(String)
+    done: Mapped[str] = mapped_column(Boolean)
+    limit: Mapped[datetime.date] = mapped_column(Date)
+
+# DB接続とセッション作成
+engine = create_engine("sqlite:///todo.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# テーブル作成
+Base.metadata.create_all(engine)
+
 # ----------------------------
 # メイン処理
 # ----------------------------
 
 # 空のtodoリスト
 # detaleの欄を追加　
-todos = [{"task": "Sample_todo", "detail": None, "done": False, "limit": None}]
+# todos = [{"task": "Sample_todo", "detail": None, "done": False, "limit": None}]
+todos = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -46,14 +77,39 @@ def index():
         todo_detail = form.todo_detail.data
         limit_date = form.limit_date.data
         if todo:  # 空でないときだけ追加
-            todos.append({"task": todo, "detail": todo_detail if todo_detail else None, 
-                        "done": False, "limit": limit_date if limit_date else None})
+            # 追加するtodoの辞書
+            todo_add_dict = {"task": todo, 
+                        "detail": todo_detail if todo_detail else None, 
+                        "done": False, 
+                        "limit": limit_date if limit_date else None}
+            todos.append(todo_add_dict)
             logger.info(f"todosの追加: {todos}")
+
+
+            # 宿題
+            # データベースへの追加
+            for t in todo_add_dict:
+                print(f"{todo_add_dict[t]}: {type(todo_add_dict[t])}")
+            session.add(TODO_DB(**todo_add_dict))
+            session.commit()
+
+            
+
             # if  not todo_detale:
+        # データベースからデータの呼び出し
+        all_data = session.query(TODO_DB).all()
+        for task_data in all_data:
+            todos.append({"id": task_data.id,
+                            "task": task_data.task,
+                            "detail": task_data.detail,
+                            "done": task_data.done,
+                            "limit": task_data.limit})
         return render_template("see_todo.html", todos=todos, form=form)
     else:
         print(form.errors)
     return render_template("index.html",  form=form)
+
+
 
 
 # @app.route("/add", methods=["POST"])
